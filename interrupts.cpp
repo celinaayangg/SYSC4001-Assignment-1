c7e1c01451b89b2f458ba60f1c39865a84461be4
Celina Yang <162600157+celinaayangg@users.noreply.github.com>
2025-09-30 13:31:27 -0400

    Modified interrupts.cpp to implement full interrupt simulation

--- interrupts.cpp
+++ interrupts.cpp
@@ -1,7 +1,7 @@
 /**
  *
  * @file interrupts.cpp
- * @author Sasisekhar Govind
+ * @author Celina Yang
  *
  */
 
@@ -19,8 +19,18 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
+	
+	int now_ms = 0; // simulation clock
 
+	const int KERNEL_MODE = 1; // switch to/from kernel mode with 1ms duration
+	const int CTX_RSTR = 10; // save/restore context with 10ms duration
+	const int ISR_START_ADRS = 1; // calculate memory of the ISR start address with 1ms duration
+	const int GET_ISR_ADRS = 1; // get ISR address from vector table with 1ms duration
+	const int EXEC_ISR = 40; // execute ISR body with 40ms duration
+	const int EXEC_IRET = 1; // execute IRET with 1ms duration
 
+	const int DEVICE_COUNT = 32; // number of devices
+	std::vector<int> device_finish(DEVICE_COUNT, -1);
 
     /******************************************************************/
 
@@ -30,8 +40,71 @@ int main(int argc, char** argv) {
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
+        if (activity == "CPU") {
+            execution += std::to_string(now_ms) + ", " + std::to_string(duration_intr) + ", CPU burst\n";
+            now_ms += duration_intr;
+        }
+	    else if (activity == "SYSCALL") {
+	   
+            // switching to kernel mode
+            execution += std::to_string(now_ms) + ", " + std::to_string(KERNEL_MODE) + ", switch to kernel mode\n";
+            now_ms += KERNEL_MODE;
 
+            // save context
+            execution += std::to_string(now_ms) + ", " + std::to_string(CTX_RSTR) + ", context saved\n";
+            now_ms += CTX_RSTR;
+            
+            // calculate ISR start address
+	        int mem_pos = ADDR_BASE + duration_intr * VECTOR_SIZE;
+	        execution += std::to_string(now_ms) + ", " + std::to_string(ISR_START_ADRS) + ", find vector " + std::to_string(duration_intr) + " in memory position " + std::to_string(mem_pos) + "\n";
+	        now_ms += ISR_START_ADRS;
 
+	        // get ISR address from vector table
+	        execution += std::to_string(now_ms) + ", " + std::to_string(GET_ISR_ADRS) + ", obtain ISR address (" + vectors.at(duration_intr) + ")\n";
+	        now_ms += GET_ISR_ADRS;
+
+	        // execute ISR body
+            execution += std::to_string(now_ms) + ", " + std::to_string(delays.at(duration_intr)) + ", call device driver\n";
+            now_ms += delays.at(duration_intr);
+	        device_finish[duration_intr] = now_ms;	
+
+            // execute IRET
+            execution += std::to_string(now_ms) + ", " + std::to_string(EXEC_IRET) + ", IRET\n";
+            now_ms += EXEC_IRET;
+        }
+	    else if (activity == "END_IO") {
+	   
+            if(device_finish[duration_intr] >= 0 && now_ms < device_finish[duration_intr]) {
+                now_ms = device_finish[duration_intr];
+            }
+            
+            device_finish[duration_intr] = -1; // reset the device
+
+            // switching to kernel mode
+            execution += std::to_string(now_ms) + ", " + std::to_string(KERNEL_MODE) + ", switch to kernel mode\n";
+            now_ms += KERNEL_MODE;
+
+            // save context
+            execution += std::to_string(now_ms) + ", " + std::to_string(CTX_RSTR) + ", context saved\n";
+            now_ms += CTX_RSTR;
+
+            // calculate ISR start address
+            int mem_pos = ADDR_BASE + duration_intr * VECTOR_SIZE;
+            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_START_ADRS) + ", find vector " + std::to_string(duration_intr) + " in memory position " + std::to_string(mem_pos) + "\n";
+            now_ms += ISR_START_ADRS;
+
+            // get ISR address from vector table
+            execution += std::to_string(now_ms) + ", " + std::to_string(GET_ISR_ADRS) + ", obtain ISR address (" + vectors.at(duration_intr) + ")\n";
+            now_ms += GET_ISR_ADRS;
+
+            // execute ISR body
+            execution += std::to_string(now_ms) + ", " + std::to_string(delays.at(duration_intr)) + ", end of I/O " + std::to_string(duration_intr) + ": interrupt\n";
+            now_ms += delays.at(duration_intr);
+	
+            // execute IRET
+            execution += std::to_string(now_ms) + ", " + std::to_string(EXEC_IRET) + ", IRET\n";
+            now_ms += EXEC_IRET;
+        }
         /************************************************************************/
 
     }
